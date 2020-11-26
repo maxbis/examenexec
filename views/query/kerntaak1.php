38d7522fe48f8d9588325eff3e1e0f76367087a9
maxbis <max@ffkijken.com>
2020-11-26 23:28:43 +0100

    stripes in results

--- views/query/kerntaak1.php
+++ views/query/kerntaak1.php
@@ -3,12 +3,25 @@ use yii\helpers\Url;
 $nr=0;
 ?>
 
+<style>
+.uneven {
+    text-align:center;
+    background-color:#F0F0F0;
+    width:50px;
+}
+.even {
+    text-align:center;
+    background-color:#F4F4F4;
+    width: 50px;
+}
+</style>
+
 <h1>Uitslagen Kerntaak-1</h1>
 <i>(Cijfers zijn berekend en kunnen maximaal 0.1 punt verschillen van de SPL cijfertabellen)</i>
 
 <p></p>
 
-<div class="card"  style="width: 800px">
+<div class="card" style="width: 700px">
     <div class="card-body">
         <table class="table">
             <thead>
@@ -21,17 +34,17 @@ $nr=0;
  
                 </tr>    
                 <tr>
-                    <th>#</th>
-                    <th>Naam</th>
-                    <th style="width: 50px;background-color:#F0F0F0;">W1</th>
-                    <th style="width: 50px;background-color:#F0F0F0;">W2</th>
-                    <th style="width: 50px;background-color:#F0F0F0;">W3</th>
-                    <th style="width: 50px;background-color:#F0F0F0;">W4</th>
+                    <th></th>
+                    <th>Kandidaat</th>
+                    <th class="uneven">W1</th>
+                    <th class="even">W2</th>
+                    <th class="uneven">W3</th>
+                    <th class="even"">W4</th>
                     <th>&nbsp;</th>
-                    <th style="width: 50px;">W1</th>
-                    <th style="width: 50px;">W2</th>
-                    <th style="width: 50px;">W3</th>
-                    <th style="width: 50px;">W4</th>
+                    <th class="uneven"">W1</th>
+                    <th class="even">W2</th>
+                    <th class="uneven">W3</th>
+                    <th class="even">W4</th>
                 </tr>    
             </thead>
             
@@ -41,16 +54,15 @@ $nr=0;
                     echo "<tr>";
                     echo "<td>".$nr."</td>";
                     echo "<td>".$naam."</td>";
-                    echo "<td style=\"background-color:#F0F0F0;\">".$value['B1-K1-W1'][0]."</td>";
-                    echo "<td style=\"background-color:#F0F0F0;\">".$value['B1-K1-W2'][0]."</td>";
-                    echo "<td style=\"background-color:#F0F0F0;\">".$value['B1-K1-W3'][0]."</td>";
-                    echo "<td style=\"background-color:#F0F0F0;\">".$value['B1-K1-W4'][0]."</td>";
+                    echo "<td class=\"uneven\">".$value['B1-K1-W1'][0]."</td>";
+                    echo "<td class=\"even\">".$value['B1-K1-W2'][0]."</td>";
+                    echo "<td class=\"uneven\">".$value['B1-K1-W3'][0]."</td>";
+                    echo "<td class=\"even\">".$value['B1-K1-W4'][0]."</td>";
                     echo "<td>&nbsp;</td>";
-                    echo "<td>".$value['B1-K1-W1'][1]."</td>";
-                    echo "<td>".$value['B1-K1-W2'][1]."</td>";
-                    echo "<td>".$value['B1-K1-W3'][1]."</td>";
-                    echo "<td>".$value['B1-K1-W4'][1]."</td>";
-
+                    echo "<td class=\"uneven\">".$value['B1-K1-W1'][1]."</td>";
+                    echo "<td class=\"even\">".$value['B1-K1-W2'][1]."</td>";
+                    echo "<td class=\"uneven\">".$value['B1-K1-W3'][1]."</td>";
+                    echo "<td class=\"even\">".$value['B1-K1-W4'][1]."</td>";
                     echo "</tr>";
                 }
             ?>
